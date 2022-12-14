7c8e2aba8993a87074efbe5a719efaf2cccb7aa6
maroosek <kluk.marek1@gmail.com>
2022-12-14 20:36:44 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import Controller from './interfaces/controller.interface';
+
+class App {
+    public app: express.Application;
+
+    constructor(controllers: Controller[]) {
+        this.app = express();
+
+        this.connectToTheDatabase();
+        this.initializeMiddlewares();
+        this.initializeControllers(controllers);
+    }
+
+    public listen() {
+        this.app.listen(process.env.PORT, () => {
+            console.log(`App listening on the port ${process.env.PORT}`);
+        });
+    }
+
+    private initializeMiddlewares() {
+        this.app.use(bodyParser.json());
+    }
+
+    private initializeControllers(controllers: Controller[]) {
+        controllers.forEach((controller) => {
+            this.app.use('/', controller.router);
+        });
+    }
+
+    private connectToTheDatabase() {
+        const {
+            MONGO_USER,
+            MONGO_PASSWORD,
+        } = process.env;
+        mongoose.connect(`mongodb+srv://maroosek:Kupeczka1@cluster0.gw3n7mq.mongodb.net/?retryWrites=true&w=majority`);
+    }
+}
+
+export default App;
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+
+import 'dotenv/config';
+import App from './app';
+import PostsController from './posts/posts.controller';
+import validateEnv from './utils/validateEnv';
+
+validateEnv();
+
+const app = new App(
+    [
+        new PostsController(),
+    ],
+);
+
+app.listen();
+
